508e1f3f638c78c1219cd232c506d11c23bf43b3
Yassine Marroun <el.yasin@hotmail.com>
2021-07-02 15:54:31 +0200

    Commit

--- 00.PythonBasics/functions.py
+++ 00.PythonBasics/functions.py
@@ -6,3 +6,30 @@ def suma(num1, num2):
 
 store_result = suma(5, 7)
 print(store_result)
+
+# lists
+myList1 = ["Peter", "Ana", "Michael", "Thomas"]
+myList2 = ["Erik", 8, 2.16]
+myList3 = myList1 + myList2
+print(myList3[:])
+
+# tuples
+myTuple = ("Paul", 12, 2, 1992, 12)
+print(myTuple[3])
+
+myList = list(myTuple)
+print(myTuple)
+
+myTuple = tuple(myList)
+print(myList)
+
+print("Paul" in myTuple)
+print(myTuple.count(12))
+print(len(myTuple))
+
+myTuple2 = ("David", 24, 9, 1985)
+name, day, month, year = myTuple2
+
+print(myTuple2)
+print(name)
+print(year)
