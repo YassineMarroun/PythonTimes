f1c11ae335819cf425f2492a6d7df04f9200488f
Yassine Marroun <el.yasin@hotmail.com>
2021-07-03 10:52:01 +0200

    Commit

--- 00.PythonBasics/Conditionals.py
+++ 00.PythonBasics/Conditionals.py
@@ -1,3 +1,4 @@
+
 print("Student marks evaluation program")
 student_mark = input("Enter the student's grade: ")
 
@@ -10,3 +11,19 @@ def evaluation(mark):
 
 
 print(evaluation(int(student_mark)))
+
+
+print("Marks verification")
+student_mark = int(input("Enter your mark, please: "))
+
+if student_mark < 5:
+    print("Failure")
+elif student_mark < 6:
+    print("Approved")
+elif student_mark < 7:
+    print("Good")
+elif student_mark < 9:
+    print("Remarkable")
+else:
+    print("Outstanding")
+print("The program has ended")
