38ac712157c6fb37b9073c8bf03882445043e3a4
Yassine Marroun <el.yasin@hotmail.com>
2021-07-03 23:13:44 +0200

    Commit

--- 00.PythonBasics/Conditionals.py
+++ 00.PythonBasics/Conditionals.py
@@ -1,4 +1,3 @@
-
 print("Student marks evaluation program")
 student_mark = input("Enter the student's grade: ")
 
@@ -27,3 +26,29 @@ elif student_mark < 9:
 else:
     print("Outstanding")
 print("The program has ended")
+
+
+age = -7
+
+if 0 < age < 100:
+    print("Age is correct")
+else:
+    print("Age is incorrect")
+
+
+president_salary = int(input("Enter the president's salary: "))
+print("President salary: " + str(president_salary))
+
+director_salary = int(input("Enter the director's salary: "))
+print("Director salary: " + str(director_salary))
+
+area_manager_salary = int(input("Enter the salary of the area manager: "))
+print("Area manager salary: " + str(area_manager_salary))
+
+administrative_salary = int(input("Enter the salary of the administrative: "))
+print("Administrative salary: " + str(administrative_salary))
+
+if administrative_salary < area_manager_salary < director_salary < president_salary:
+    print("Everything works correctly")
+else:
+    print("Something is wrong with the accounts of the company")
