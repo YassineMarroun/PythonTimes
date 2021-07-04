fdd35fc2ae163bca3d1eb7e050a37e20c4e8b376
Yassine Marroun <el.yasin@hotmail.com>
2021-07-04 16:36:14 +0200

    Commit

--- 00.PythonBasics/Loops.py
+++ 00.PythonBasics/Loops.py
@@ -3,3 +3,22 @@ for i in [1, 2, 3]:
 
 for j in ["one", "two", "three", "four"]:
     print(j)
+
+for k in ["one", "two", "three", "four"]:
+    print(k, end=" ")
+
+for m in "number_of_characters":
+    print("Letter", end=" ")
+
+
+counter = 0
+myEmail = input("Enter your email address: ")
+
+for i in myEmail:
+    if i == "@" or i == ".":
+        counter = counter + 1
+
+if counter == 2:
+    print("Email is correct")
+else:
+    print("Email is incorrect")
