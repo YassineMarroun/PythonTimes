d0744ea3c06b8b9665d205d12cee7c45355a8c06
Yassine Marroun <el.yasin@hotmail.com>
2021-07-05 09:25:03 +0200

    Commit

--- 00.PythonBasics/Loops.py
+++ 00.PythonBasics/Loops.py
@@ -1,3 +1,6 @@
+import math
+
+# For loop
 for i in [1, 2, 3]:
     print("Hello")
 
@@ -9,7 +12,6 @@ for k in ["one", "two", "three", "four"]:
 
 for m in "number_of_characters":
     print("Letter", end=" ")
-
 
 counter = 0
 myEmail = input("Enter your email address: ")
@@ -23,10 +25,8 @@ if counter == 2:
 else:
     print("Email is incorrect")
 
-
 for c in range(5, 50, 3):
     print(f"Variable value {c}")
-
 
 valid = False
 myEmail = input("Enter your email address: ")
@@ -38,3 +38,39 @@ if valid:
     print("Email is correct")
 else:
     print("Email is incorrect")
+
+
+# While loop
+i = 1
+while i <= 10:
+    print("Execution " + str(i))
+    i = i + 1
+print("Execution ended")
+
+age = int(input("Enter your age, please: "))
+while age < 5 or age > 100:
+    print("You have entered an incorrect age. Try again")
+    age = int(input("Enter your age, please: "))
+
+print("Thanks for your cooperation, you can pass.")
+print("Applicant's age: " + str(age))
+
+
+print("Square root calculation program")
+number = int(input("Please enter a number: "))
+attempts = 0
+
+while number < 0:
+    print("Cannot calculate the root of a negative number")
+
+    if attempts == 2:
+        print("You have consumed too many attempts. The program has ended.")
+        break
+
+    number = int(input("Please enter a new number: "))
+    if number < 0:
+        attempts = attempts + 1
+
+if attempts < 2:
+    solution = math.sqrt(number)
+    print("The square root of " + str(number) + " is " + str(solution))
