cc7760807d6a51b034b7ed9b004140cdf0944d95
Yassine Marroun <el.yasin@hotmail.com>
2021-07-06 23:59:23 +0200

    Commit

--- 00.PythonBasics/POOExample.py
+++ 00.PythonBasics/POOExample.py
@@ -10,13 +10,32 @@ class Car:
         self.__ongoing = starting
 
         if self.__ongoing:
+            check = self.__internal_check()
+
+        if self.__ongoing and check:
             return "The car is running."
+
+        elif self.__ongoing and check == False:
+            return "Something went wrong with the checkup. The car cannot start"
+
         else:
             return "The car is stopped."
 
     def state(self):
         print("The car has", self.__wheels, "wheels. A width of", self.__widthChassis,
               "and a length of", self.__longChassis)
+
+    def __internal_check(self):
+        print("Performing internal check")
+
+        self.gasoline = "Ok"
+        self.oil = "Ok"
+        self.doors = "Closed"
+
+        if self.gasoline == "Ok" and self.oil == "Ok" and self.doors == "Closed":
+            return True
+        else:
+            return False
 
 
 myCar = Car()
@@ -27,5 +46,4 @@ print("-- Next we create a second object -- ")
 
 myCar2 = Car()
 print(myCar2.start(False))
-myCar2.wheels = 2
 myCar2.state()
