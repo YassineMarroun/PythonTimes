5f5d8c24fe14eabec0cf33b00b50f7fb99236c6c
Yassine Marroun <el.yasin@hotmail.com>
2021-07-06 20:30:21 +0200

    Commit

--- 00.PythonBasics/POOExample.py
+++ 00.PythonBasics/POOExample.py
@@ -1,21 +1,31 @@
 class Car:
-    longChassis = 250
-    widthChassis = 120
-    wheels = 4
-    ongoing = False
 
-    def start(self):
-        self.ongoing = True
+    def __init__(self):
+        self.__longChassis = 250
+        self.__widthChassis = 120
+        self.__wheels = 4
+        self.__ongoing = False
 
-    def state(self):
-        if self.ongoing:
+    def start(self, starting):
+        self.__ongoing = starting
+
+        if self.__ongoing:
             return "The car is running."
         else:
             return "The car is stopped."
 
+    def state(self):
+        print("The car has", self.__wheels, "wheels. A width of", self.__widthChassis,
+              "and a length of", self.__longChassis)
+
 
 myCar = Car()
-print("The length of the car is:", myCar.longChassis)
-print("The car has", myCar.wheels, "wheels")
-myCar.start()
-print(myCar.state())
+print(myCar.start(True))
+myCar.state()
+
+print("-- Next we create a second object -- ")
+
+myCar2 = Car()
+print(myCar2.start(False))
+myCar2.wheels = 2
+myCar2.state()
