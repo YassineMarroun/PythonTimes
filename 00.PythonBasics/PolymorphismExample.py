abe62e8e40001cd92e8e53f6efbb720bc920609f
Yassine Marroun <el.yasin@hotmail.com>
2021-07-11 15:42:27 +0200

    Commit

---
+++ 00.PythonBasics/PolymorphismExample.py
@@ -0,0 +1,20 @@
+class Car:
+
+    def displacement(self):
+        print("I move using four wheels.")
+
+
+class Motorcycle:
+
+    def displacement(self):
+        print("I move using two wheels.")
+
+
+class Truck:
+
+    def displacement(self):
+        print("I move using two wheels.")
+
+
+myVehicle = Motorcycle()
+myVehicle.displacement()
