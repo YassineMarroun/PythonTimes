7545ae9d1498d3247c7341cc1bb1abd797e8bb92
Yassine Marroun <el.yasin@hotmail.com>
2021-07-11 19:34:42 +0200

    Commit

---
+++ 01.Modules/MathFunctions.py
@@ -0,0 +1,10 @@
+def addition(op1, op2):
+    print("The result of the addition is: ", op1 + op2)
+
+
+def subtraction(op1, op2):
+    print("The result of the subtraction is: ", op1 - op2)
+
+
+def multiplication(op1, op2):
+    print("The result of the multiplication is: ", op1 * op2)
---
+++ 01.Modules/UsingFunctions.py
@@ -0,0 +1,7 @@
+# import MathFunctions
+
+from MathFunctions import *
+
+addition(7, 5)
+subtraction(9, 5)
+multiplication(3, 5)
