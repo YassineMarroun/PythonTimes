2fc7e1268f8baf165e74185aae94b302ccb6af09
Yassine Marroun <el.yasin@hotmail.com>
2021-07-12 17:23:07 +0200

    Commit

---
+++ 01.Modules/VehicleModule.py
@@ -0,0 +1,50 @@
+class Vehicles:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+        self.ongoing = False
+        self.accelerate = False
+        self.brake = False
+
+    def start(self):
+        self.ongoing = True
+
+    def speed_up(self):
+        self.accelerate = True
+
+    def slow_down(self):
+        self.brake = True
+
+    def state(self):
+        print("Brand:", self.brand, "\nModel:", self.model, "\nOn going:", self.ongoing,
+              "\nAccelerating:", self.accelerate, "\nBraking:", self.brake)
+
+
+class Van(Vehicles):
+
+    def load(self, loading):
+        self.loaded = loading
+        if self.loaded:
+            return "The van is loaded."
+        else:
+            return "The van is not loaded."
+
+
+class Motorcycle(Vehicles):
+    doing_wheelie = ""
+
+    def wheelie(self):
+        self.doing_wheelie = "Bike doing wheelie."
+
+    def state(self):
+        print("Brand:", self.brand, "\nModel:", self.model, "\nOn going:", self.ongoing,
+              "\nAccelerating:", self.accelerate, "\nBraking:", self.brake, "\n", self.doing_wheelie)
+
+
+class ElectricVehicles(Vehicles):
+    def __init__(self, brand, model):
+        super().__init__(brand, model)
+        self.autonomy = 100
+
+    def charge_energy(self):
+        self.charging = True
---
+++ 01.Modules/VehicleUse.py
@@ -0,0 +1,4 @@
+from VehicleModule import *
+
+myCar = Vehicles("Mazda", "MX5")
+myCar.state()
