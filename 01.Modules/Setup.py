16a6ed5a031248b7dfd2d5ce616e44926bd5164f
Yassine Marroun <el.yasin@hotmail.com>
2021-07-19 13:58:34 +0200

    Commit

---
+++ 01.Modules/Setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+
+    name="CalculationPackage",
+    version="1.0",
+    description="Rounding and Potency Package",
+    author="Yassine",
+    author_email="el.yasin@hotmail.com",
+    url="https://github.com/YassineMarroun",
+    packages=["Calculations", "Calculations.RoundingPotency"]
+)
