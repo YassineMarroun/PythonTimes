e4e89a4571bffdf475f63a277ff90bda0ea135b2
Yassine Marroun <el.yasin@hotmail.com>
2021-07-19 20:41:38 +0200

    Commit

--- 01.Modules/FileHandling.py
+++ 01.Modules/FileHandling.py
@@ -7,6 +7,7 @@ text_file.write(sentence
 text_file.close()
 """
 
+
 """
 text_file = open("file.txt", "r")
 
@@ -18,6 +19,33 @@ print(text)
 # print(text_lines[0])
 """
 
+
+"""
 text_file = open("file.txt", "a")
-text_file.write("\n It is always a good time to study Python")
+text_file.write("\nIt is always a good time to study Python")
+text_file.close()
+"""
+
+
+"""
+text_file = open("file.txt", "r")
+print(text_file.read())
+print("-------------------------")
+text_file.seek(10)
+print(text_file.read())
+print("-------------------------")
+text_file.seek(len(text_file.read())/2)
+print(text_file.read())
+"""
+
+
+text_file = open("file.txt", "r+")  # Reading and writing
+text_file.write("Beginning of the text")
+print(text_file.readlines())
+print("-------------------------")
+text_file.seek(0)
+text_list = text_file.readlines()
+text_list[1] = "This line has been included from outside\n"
+text_file.seek(0)
+text_file.writelines(text_list)
 text_file.close()
