912ac0fb1b4883990f055a67ef60bab8aa39952c
Yassine Marroun <el.yasin@hotmail.com>
2021-07-25 18:41:45 +0200

    Commit

--- 01.Modules/PermanentInfo.py
+++ 01.Modules/PermanentInfo.py
@@ -30,18 +30,30 @@ class PeopleList:
 
     def add_people(self, person):
         self.people.append(person)
+        self.save_people_in_external_file()
 
     def show_people(self):
         for person in self.people:
             print(person)
 
+    def save_people_in_external_file(self):
+        list_of_people = open("externalFile", "wb")
+        pickle.dump(self.people, list_of_people)
+        list_of_people.close()
+        del list_of_people
+
+    def show_external_file_info(self):
+        print("The information in the external file is as follows:")
+        for p in self.people:
+            print(p)
+
 
 myList = PeopleList()
-"""
 p = Person("Anna", "Female", 29)
 myList.add_people(p)
 p = Person("Tonny", "Male", 39)
 myList.add_people(p)
 p = Person("Rachael", "Female", 19)
 myList.add_people(p)
-"""
+
+myList.show_external_file_info()
