1295ff7aaec10510b75bb415dbb6b59bf2f481b6
Yassine Marroun <el.yasin@hotmail.com>
2021-08-12 01:21:55 +0200

    Commit

---
+++ 02.GraphicalInterfaces/FirstInterface.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+root = Tk()
+root.title("Testing window")
+root.resizable(True, False)
+
+# root.iconbitmap("cats.ico")
+root.geometry("650x350")
+root.config(bg="blue")
+
+root.mainloop()
