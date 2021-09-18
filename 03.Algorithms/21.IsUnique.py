e4b65cf70476d5a0cbbad15e32b2e85d4313517e
YassineMarroun <el.yasin@hotmail.com>
2021-09-18 23:06:41 +0200

    Commit

--- 03.Algorithms/21.IsUnique.py
+++ 03.Algorithms/21.IsUnique.py
@@ -2,20 +2,51 @@
 Algorithm to determine if a string has all unique characters.
 """
 
-
-# "unique"
-# "bear"
-
-
-def is_unique(s):
-    return len(s) == len(set(s))
+unique_str = "AbCdEfG"
+non_unique_str = "non Unique STR"
 
 
-s1 = "unique"
-s2 = "bear"
+def normalize(input_str):
+    input_str = input_str.replace(" ", "")
+    return input_str.lower()
 
-# False
-print(is_unique(s1))
 
-# True
-print(is_unique(s2))
+def is_unique1(input_str):
+    d = dict()
+    for i in input_str:
+        if i in d:
+            return False
+        else:
+            d[i] = 1
+    return True
+
+
+def is_unique2(input_str):
+    return len(input_str) == len(set(input_str))
+
+
+def is_unique3(input_str):
+    alpha = "abcdefghijklmnopqrstuvwxyz"
+
+    for i in input_str:
+        if i in alpha:
+            alpha = alpha.replace(i, "")
+        else:
+            return False
+    return True
+
+
+unique_str = normalize(unique_str)
+non_unique_str = normalize(non_unique_str)
+
+print(unique_str)
+print(non_unique_str)
+
+print(is_unique1(unique_str))
+print(is_unique1(non_unique_str))
+
+print(is_unique2(unique_str))
+print(is_unique2(non_unique_str))
+
+print(is_unique3(unique_str))
+print(is_unique3(non_unique_str))
