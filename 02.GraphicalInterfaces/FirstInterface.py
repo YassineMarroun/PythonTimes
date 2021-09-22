189c0c67fd35eb93f5c0cf5f85a825d6457dec6c
YassineMarroun <el.yasin@hotmail.com>
2021-09-22 09:47:09 +0200

    Commit

--- 02.GraphicalInterfaces/FirstInterface.py
+++ 02.GraphicalInterfaces/FirstInterface.py
@@ -2,10 +2,19 @@ from tkinter import *
 
 root = Tk()
 root.title("Testing window")
-root.resizable(True, False)
+# root.resizable(True, False)
 
 # root.iconbitmap("cats.ico")
 root.geometry("650x350")
 root.config(bg="blue")
+
+myFrame = Frame()
+myFrame.pack(side="left", anchor="n")
+# myFrame.pack(fill="x", expand=True)
+myFrame.config(bg="red")
+myFrame.config(width="650", height="350")
+myFrame.config(bd=35)
+myFrame.config(relief="groove")
+myFrame.config(cursor="pirate")
 
 root.mainloop()
