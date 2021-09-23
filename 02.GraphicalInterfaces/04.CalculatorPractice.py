73b95d93e2c32b7fa3405b7eec2a335acd635fff
YassineMarroun <el.yasin@hotmail.com>
2021-09-23 12:12:40 +0200

    Commit

--- 02.GraphicalInterfaces/04.CalculatorPractice.py
+++ 02.GraphicalInterfaces/04.CalculatorPractice.py
@@ -8,18 +8,26 @@ myFrame.pack()
 
 # ----------- Screen -----------
 
-screen = Entry(myFrame)
+numberScreen = StringVar()
+
+screen = Entry(myFrame, textvariable=numberScreen)
 screen.grid(row=1, column=1, padx=10, pady=10, columnspan=4)
 screen.config(background="black", fg="#03f943", justify="right")
 
 
+# ----------- Keystrokes -----------
+
+def numberPressed(num):
+    numberScreen.set(numberScreen.get() + num)
+
+
 # ----------- Row 1 -----------
 
-button7 = Button(myFrame, text="7", width=3)
+button7 = Button(myFrame, text="7", width=3, command=lambda:numberPressed("7"))
 button7.grid(row=2, column=1)
-button8 = Button(myFrame, text="8", width=3)
+button8 = Button(myFrame, text="8", width=3, command=lambda:numberPressed("8"))
 button8.grid(row=2, column=2)
-button9 = Button(myFrame, text="9", width=3)
+button9 = Button(myFrame, text="9", width=3, command=lambda:numberPressed("9"))
 button9.grid(row=2, column=3)
 buttonDiv = Button(myFrame, text="/", width=3)
 buttonDiv.grid(row=2, column=4)
@@ -27,11 +35,11 @@ buttonDiv.grid(row=2, column=4)
 
 # ----------- Row 2 -----------
 
-button4 = Button(myFrame, text="4", width=3)
+button4 = Button(myFrame, text="4", width=3, command=lambda:numberPressed("4"))
 button4.grid(row=3, column=1)
-button5 = Button(myFrame, text="5", width=3)
+button5 = Button(myFrame, text="5", width=3, command=lambda:numberPressed("5"))
 button5.grid(row=3, column=2)
-button6 = Button(myFrame, text="6", width=3)
+button6 = Button(myFrame, text="6", width=3, command=lambda:numberPressed("6"))
 button6.grid(row=3, column=3)
 buttonMult = Button(myFrame, text="x", width=3)
 buttonMult.grid(row=3, column=4)
@@ -39,11 +47,11 @@ buttonMult.grid(row=3, column=4)
 
 # ----------- Row 3 -----------
 
-button1 = Button(myFrame, text="1", width=3)
+button1 = Button(myFrame, text="1", width=3, command=lambda:numberPressed("1"))
 button1.grid(row=4, column=1)
-button2 = Button(myFrame, text="2", width=3)
+button2 = Button(myFrame, text="2", width=3, command=lambda:numberPressed("2"))
 button2.grid(row=4, column=2)
-button3 = Button(myFrame, text="3", width=3)
+button3 = Button(myFrame, text="3", width=3, command=lambda:numberPressed("3"))
 button3.grid(row=4, column=3)
 buttonSub = Button(myFrame, text="-", width=3)
 buttonSub.grid(row=4, column=4)
@@ -51,9 +59,9 @@ buttonSub.grid(row=4, column=4)
 
 # ----------- Row 4 -----------
 
-button0 = Button(myFrame, text="0", width=3)
+button0 = Button(myFrame, text="0", width=3, command=lambda:numberPressed("0"))
 button0.grid(row=5, column=1)
-buttonComma = Button(myFrame, text=",", width=3)
+buttonComma = Button(myFrame, text=",", width=3, command=lambda:numberPressed(","))
 buttonComma.grid(row=5, column=2)
 buttonEqual = Button(myFrame, text="=", width=3)
 buttonEqual.grid(row=5, column=3)
